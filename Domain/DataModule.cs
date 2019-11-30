3e8a5559dcf72f0c1fdb414041c16d047a066a3e
vladmijev <58359805+vladmijev@users.noreply.github.com>
2019-11-30 12:01:01 +0100

    Add files via upload

---
+++ Domain/DataModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Domain.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Module = Autofac.Module;
+
+namespace Domain
+{
+	public class DataModule: Module
+	{
+		private readonly IServiceCollection services;
+
+		public DataModule(IServiceCollection services)
+		{
+			this.services = services;
+		}
+
+		protected override void Load(ContainerBuilder builder)
+		{
+			LoadDbContextsIntoServices();
+			builder.RegisterType<AdventureWorks2016Context>().AsSelf().InstancePerLifetimeScope();
+		}
+
+		private void LoadDbContextsIntoServices()
+		{
+			services.AddDbContext<AdventureWorks2016Context>();
+		}
+	}
+}
---
+++ Domain/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	public interface IRepository<TObject>
+		where TObject : class
+	{
+		TObject Get(int id);
+		Task<TObject> GetAsync(int id);
+
+		ICollection<TObject> GetAll();
+		Task<ICollection<TObject>> GetAllAsync();
+
+		TObject Add(TObject item);
+		TObject Update(TObject item, int key);
+		void Delete(TObject item);
+
+		int SaveChanges();
+		Task<int> SaveCghangesAsync(CancellationToken cancellationToken);
+	}
+}
