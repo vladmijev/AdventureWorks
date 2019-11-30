351f6825d256122a5200dcff66e1331f1ea26bd7
vladmijev <58359805+vladmijev@users.noreply.github.com>
2019-11-30 11:50:32 +0100

    AdventureWorks.IntegrationTests

---
+++ AdventureWorks.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AdventureWorks.IntegrationTests
+{
+	public abstract class IntegrationTest
+	{
+
+		protected readonly HttpClient httpClient;
+
+
+		public IntegrationTest()
+		{
+			var factory = new WebApplicationFactory<Startup>();
+			httpClient = factory.CreateClient();
+		}
+
+
+	}
+}
---
+++ AdventureWorks.IntegrationTests/ProductionControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventureWorks.IntegrationTests
+{
+	[TestClass]
+	public class ProductionControllerTest : IntegrationTest
+	{
+		[TestMethod]
+		public async Task GetProduct_WithData_ReturnsOkRequest()
+		{
+
+			string requestUri = "http://localhost:1357/api/Production/GetProduct?name=test1&description=test2&sellStartDate=01.01.2019";
+
+			var response = await httpClient.GetAsync(requestUri);
+			
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+		}
+
+		[TestMethod]
+		public async Task GetProduct_WithoutData_ReturnsBadRequest()
+		{
+
+			string requestUri = "http://localhost:1357/api/Production/GetProduct";
+
+			var response = await httpClient.GetAsync(requestUri);
+
+			response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+		}
+
+		[TestMethod]
+		public async Task GetPurchaseOrderDetail_WithoutData_ReturnsBadRequest()
+		{
+
+			string requestUri = "http://localhost:1357/api/Production/GetPurchaseOrderDetail";
+
+			var response = await httpClient.GetAsync(requestUri);
+
+			response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+		}
+
+		[TestMethod]
+		public async Task GetPurchaseOrderDetail_WithData_ReturnsOkRequest()
+		{
+
+			string requestUri = "http://localhost:1357/api/Production/GetPurchaseOrderDetail?startTime=01.11.2019&endTime=01.11.2019";
+
+			var response = await httpClient.GetAsync(requestUri);
+
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+		}
+	}
+}
